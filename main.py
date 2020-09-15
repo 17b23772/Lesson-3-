c5355c93498eacc07e751cab9185e4e5bc909b60
Oliver Beach <17b23772@marling.school>
2020-09-15 10:05:24 +0000

    All nouns and verbs and adjectives

---
+++ main.py
@@ -0,0 +1,8 @@
+noun = "bike"
+adverb = "quickly"
+adjective = "quiet"
+print(f"The {noun} was {adjective} when it {adverb} went to school")
+noun = "donkey"
+adverb = "moonwalked"
+adjective = "vibing"
+print(f"The {noun} was {adjective} when it {adverb} to school")
